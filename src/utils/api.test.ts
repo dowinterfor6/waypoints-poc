5aaaa3ff214cdf0b56a082a32ccf1c0af3a522d2
dowinterfor6 <dowinterfor6@gmail.com>
2025-02-28 21:20:45 +0800

    Add getRoutePathByToken and retry strategy, add unit tests

--- src/utils/api.test.ts
+++ src/utils/api.test.ts
@@ -1,96 +1,235 @@
 import { describe, expect, spyOn, test } from "bun:test";
-import { postMockApi } from "./api";
+import { getRoutePathByToken, getRouteToken } from "./api";
 
 describe("api", () => {
-  // I used this to do TDD to make sure I get the right API response shape
-  test.skip("post /route returns a token on success, or error message on error", async () => {
-    const res = await postMockApi("asdf", "asdf2");
-
-    try {
-      // Test success state
-      expect(res).toEqual({ status: "SUCCESS", token: expect.any(String) });
-    } catch (_) {
-      // Test error state
-      expect(res).toEqual({
-        status: "ERROR",
-        errorMessage: expect.any(String),
-      });
-    }
-  });
-
-  test("post /route returns a token on success", async () => {
-    spyOn(global, "fetch").mockImplementationOnce(() =>
-      Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve({ token: "validToken" }),
-      } as Response)
-    );
-
-    const res = await postMockApi("asdf", "asdf2");
-
-    expect(res).toEqual({ status: "SUCCESS", token: "validToken" });
-  });
-
-  describe("error states", () => {
-    test("post /route returns an error message on rejected error state", async () => {
-      spyOn(global, "fetch").mockImplementationOnce(() =>
-        Promise.reject({} as unknown as Response)
-      );
-
-      const res = await postMockApi("asdf", "asdf2");
-
-      expect(res).toEqual({
-        status: "ERROR",
-        errorMessage: expect.any(String),
-      });
-    });
-
-    test("post /route returns an error message on resolved error state", async () => {
-      spyOn(global, "fetch").mockImplementationOnce(() =>
-        Promise.resolve({
-          ok: false,
-          url: "https://sg-mock-api.lalamove.com/route",
-          status: 500,
-          statusText: "Internal Server Error",
-          headers: {
-            date: "Fri, 28 Feb 2025 09:40:24 GMT",
-            "content-type": "text/plain; charset=utf-8",
-            "content-length": "21",
-            connection: "keep-alive",
-            "access-control-allow-origin": "*",
-            "access-control-allow-methods": "GET,HEAD,PUT,POST,DELETE",
-            "waf-ray-id": "f93077ddea5932ec987f45fb6f383242",
-            "x-kong-upstream-latency": "1",
-            "x-kong-proxy-latency": "0",
-            server: "WAF-Gateway/0.37",
-            "x-apisix-upstream-status": "500",
-          },
-          redirected: false,
-          bodyUsed: false,
-        } as unknown as Response)
-      );
-
-      const res = await postMockApi("asdf", "asdf2");
-
-      expect(res).toEqual({
-        status: "ERROR",
-        errorMessage: expect.any(String),
-      });
-    });
-
-    test("post /route returns an error message on resolved error state that doesn't contain the token", async () => {
+  describe("getRouteToken", () => {
+    test("post /route returns a token on success", async () => {
       spyOn(global, "fetch").mockImplementationOnce(() =>
         Promise.resolve({
           ok: true,
-          json: () => Promise.resolve({ notTheToken: "asdf" }),
-        } as unknown as Response)
+          json: () => Promise.resolve({ token: "validToken" }),
+        } as Response)
       );
 
-      const res = await postMockApi("asdf", "asdf2");
+      const res = await getRouteToken("asdf", "asdf2");
+
+      expect(res).toEqual({ status: "SUCCESS", token: "validToken" });
+    });
+
+    describe("error states", () => {
+      test("post /route returns an error message on rejected error state", async () => {
+        spyOn(global, "fetch").mockImplementationOnce(() =>
+          Promise.reject({} as Response)
+        );
+
+        const res = await getRouteToken("asdf", "asdf2");
+
+        expect(res).toEqual({
+          status: "ERROR",
+          errorMessage: expect.any(String),
+        });
+      });
+
+      test("post /route returns an error message on resolved error state", async () => {
+        spyOn(global, "fetch").mockImplementationOnce(() =>
+          Promise.resolve({
+            ok: false,
+            url: "https://sg-mock-api.lalamove.com/route",
+            status: 500,
+            statusText: "Internal Server Error",
+            headers: {
+              date: "Fri, 28 Feb 2025 09:40:24 GMT",
+              "content-type": "text/plain; charset=utf-8",
+              "content-length": "21",
+              connection: "keep-alive",
+              "access-control-allow-origin": "*",
+              "access-control-allow-methods": "GET,HEAD,PUT,POST,DELETE",
+              "waf-ray-id": "f93077ddea5932ec987f45fb6f383242",
+              "x-kong-upstream-latency": "1",
+              "x-kong-proxy-latency": "0",
+              server: "WAF-Gateway/0.37",
+              "x-apisix-upstream-status": "500",
+            },
+            redirected: false,
+            bodyUsed: false,
+          } as unknown as Response)
+        );
+
+        const res = await getRouteToken("asdf", "asdf2");
+
+        expect(res).toEqual({
+          status: "ERROR",
+          errorMessage: expect.any(String),
+        });
+      });
+
+      test("post /route returns an error message on resolved error state that doesn't contain the token", async () => {
+        spyOn(global, "fetch").mockImplementationOnce(() =>
+          Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve({ notTheToken: "asdf" }),
+          } as Response)
+        );
+
+        const res = await getRouteToken("asdf", "asdf2");
+
+        expect(res).toEqual({
+          status: "ERROR",
+          errorMessage: expect.any(String),
+        });
+      });
+    });
+  });
+
+  describe("getRoutePathByToken", () => {
+    const resolvedData = {
+      path: [
+        ["22.372081", "114.107877"],
+        ["22.326442", "114.167811"],
+        ["22.284419", "114.159510"],
+      ] as Array<[string, string]>,
+      total_distance: 20000,
+      total_time: 1800,
+    };
+
+    test("get /route/:token returns path, total distance, and total time on success", async () => {
+      spyOn(global, "fetch").mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(resolvedData),
+        } as Response)
+      );
+
+      const res = await getRoutePathByToken("asdf");
+
+      const {
+        path,
+        total_distance: totalDistance,
+        total_time: totalTime,
+      } = resolvedData;
 
       expect(res).toEqual({
-        status: "ERROR",
-        errorMessage: expect.any(String),
+        status: "SUCCESS",
+        path,
+        totalDistance,
+        totalTime,
+      });
+    });
+
+    describe("'in progress' status retries", () => {
+      test("get /route/:token retries until success on 'in progress' resolved status", async () => {
+        spyOn(global, "fetch")
+          .mockImplementationOnce(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ status: "in progress" }),
+            } as Response)
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ status: "in progress" }),
+            } as Response)
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ status: "in progress" }),
+            } as Response)
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve(resolvedData),
+            } as Response)
+          );
+
+        const res = await getRoutePathByToken("asdf");
+
+        const {
+          path,
+          total_distance: totalDistance,
+          total_time: totalTime,
+        } = resolvedData;
+
+        expect(res).toEqual({
+          status: "SUCCESS",
+          path,
+          totalDistance,
+          totalTime,
+        });
+      });
+
+      test("get /route/:token retries until error on 'in progress' resolved status", async () => {
+        spyOn(global, "fetch")
+          .mockImplementationOnce(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ status: "in progress" }),
+            } as Response)
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ status: "in progress" }),
+            } as Response)
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ status: "in progress" }),
+            } as Response)
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({
+              ok: true,
+              json: () =>
+                Promise.resolve({
+                  status: "failure",
+                  error: "Location not accessible by car",
+                }),
+            } as Response)
+          );
+
+        const res = await getRoutePathByToken("asdf");
+
+        expect(res).toEqual({
+          status: "ERROR",
+          errorMessage: expect.any(String),
+        });
+      });
+    });
+
+    describe("error states", () => {
+      test("get /route/:token returns an error message on rejected error state", async () => {
+        spyOn(global, "fetch").mockImplementationOnce(() => Promise.reject({}));
+
+        const res = await getRouteToken("asdf", "asdf2");
+
+        expect(res).toEqual({
+          status: "ERROR",
+          errorMessage: expect.any(String),
+        });
+      });
+
+      test("get /route/:token returns an error message on resolved error state that has 'failure' status", async () => {
+        spyOn(global, "fetch").mockImplementationOnce(() =>
+          Promise.resolve({
+            ok: true,
+            json: () =>
+              Promise.resolve({
+                status: "failure",
+                error: "Location not accessible by car",
+              }),
+          } as Response)
+        );
+
+        const res = await getRoutePathByToken("asdf");
+
+        expect(res).toEqual({
+          status: "ERROR",
+          errorMessage: expect.any(String),
+        });
       });
     });
   });
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 const MOCK_API_BASE_URL = "https://sg-mock-api.lalamove.com";
 
-type PostResponse =
+export type PostRouteResponse =
   | {
       status: "SUCCESS";
       token: string;
@@ -10,10 +10,10 @@ type PostResponse =
       errorMessage: string;
     };
 
-export const postMockApi = async (
+export const getRouteToken = async (
   origin: string,
   destination: string
-): Promise<PostResponse> => {
+): Promise<PostRouteResponse> => {
   const apiUrl = `${MOCK_API_BASE_URL}/route`;
 
   try {
@@ -52,4 +52,68 @@ export const postMockApi = async (
   }
 };
 
-export const getRouteByToken = async () => {};
+export type GetRouteResponse =
+  | {
+      status: "SUCCESS";
+      path: Array<[string, string]>;
+      totalDistance: number;
+      totalTime: number;
+    }
+  | {
+      status: "ERROR";
+      errorMessage: string;
+    };
+
+export const getRoutePathByToken = async (
+  token: string
+): Promise<GetRouteResponse> => {
+  const apiUrl = `${MOCK_API_BASE_URL}/route/${token}`;
+
+  try {
+    const res = await fetch(apiUrl);
+
+    if (!res.ok) {
+      throw new Error(`Response status: ${res.status}`);
+    }
+
+    const json = await res.json();
+
+    if ("status" in json && json.status === "in progress") {
+      // TODO: Implement sleep?
+      return await getRoutePathByToken(token);
+    }
+
+    if (
+      !("path" in json) ||
+      !Array.isArray(json.path) ||
+      !("total_distance" in json) ||
+      typeof json.total_distance !== "number" ||
+      !("total_time" in json) ||
+      typeof json.total_time !== "number"
+    ) {
+      throw new Error(
+        "Response shape not expected, path and/or total_distance and/or total_time not found"
+      );
+    }
+
+    const { path, total_distance: totalDistance, total_time: totalTime } = json;
+
+    return {
+      status: "SUCCESS",
+      path,
+      totalDistance,
+      totalTime,
+    };
+  } catch (error) {
+    let errorMessage = "Failed to fetch route";
+
+    if (typeof error === "string") {
+      errorMessage = error;
+    }
+
+    return {
+      status: "ERROR",
+      errorMessage,
+    };
+  }
+};
