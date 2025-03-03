be9bda31b7b2d178472da75275ae8bb02f819555
dowinterfor6 <dowinterfor6@gmail.com>
2025-03-03 17:02:08 +0800

    Disable SSR output, enable static output

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,17 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
 
+import dynamic from "next/dynamic";
+import React from "react";
+
 /**
  * Used for globally accessible components, layouts, styles and structures
  */
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
+
+// Disable SSR since this is a simple SPA with lots of browser API dependency
+export default dynamic(() => Promise.resolve(App), {
+  ssr: false,
+});
