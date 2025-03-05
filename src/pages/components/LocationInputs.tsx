9abf9095e8eec958762f3ca62a7c2c7a8b5d86ad
dowinterfor6 <dowinterfor6@gmail.com>
2025-03-05 23:52:02 +0800

    Fix debounce on input autocomplete fetch

--- src/pages/components/LocationInputs.tsx
+++ src/pages/components/LocationInputs.tsx
@@ -2,7 +2,14 @@ import { getAutocompleteSuggestions } from "@/utils/mapsApi";
 import { Clear } from "@mui/icons-material";
 import { Autocomplete, TextField } from "@mui/material";
 import { debounce } from "lodash";
-import React, { Dispatch, FC, SetStateAction, useState } from "react";
+import React, {
+  Dispatch,
+  FC,
+  SetStateAction,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
 
 type Props = {
   label: string;
@@ -23,32 +30,55 @@ const LocationInput: FC<Props> = ({
     Array<string>
   >([]);
 
-  const fetchAutocompleteSuggestions = async (value: string) => {
-    if (value === "") {
-      return [];
-    }
+  /**
+   * @see https://github.com/facebook/react/issues/19240#issuecomment-652945246 for
+   * why a useMemo is needed in conjunction with useCallback
+   *
+   * TLDR: The debounced function reference changes every rerender, therefore a
+   * useCallback is needed to stop it from doing so. The eslint rule wasn't happy
+   * about a non-inline function, so adding a useMemo to evaluate a (memoized)
+   * version of the function fixes the issue.
+   */
+  const debouncedFetchAutocompleteSuggestions = useMemo(
+    () =>
+      debounce(async (value: string) => {
+        if (value === "") {
+          setAutocompleteSuggestions([]);
+          return;
+        }
 
-    const autoCompleteSuggestions = await getAutocompleteSuggestions(value);
+        const autoCompleteSuggestions = await getAutocompleteSuggestions(value);
 
-    if (!!autoCompleteSuggestions) {
-      setAutocompleteSuggestions(autoCompleteSuggestions);
-    }
-  };
+        setAutocompleteSuggestions(autoCompleteSuggestions ?? []);
+      }, 400),
+    [setAutocompleteSuggestions]
+  );
 
-  const debouncedFetchAutocompleteSuggestions = debounce(
-    (value) => fetchAutocompleteSuggestions(value),
-    400
+  const usedCallbackDebouncedFetchAutocompleteSuggestions = useCallback(
+    debouncedFetchAutocompleteSuggestions,
+    [debouncedFetchAutocompleteSuggestions]
   );
 
   return (
     <Autocomplete
       freeSolo
+      /**
+       * This MUI component has in interesting quirk that it also filters on the
+       * options at the same time the input is being changed. What this means is
+       * that there's an interesting UX where the autocomplete suggestions fetch
+       * is being debounced and waiting for the user to finish typing, but the
+       * user input is already filtering the existing `options`.
+       *
+       * Since this is a feature of the MUI component, and not my API implementation
+       * I won't address this minor UX issue.
+       */
       options={autocompleteSuggestions}
       clearIcon={<Clear />}
       inputValue={value}
       onInputChange={(_, value) => {
         setValue(value);
-        debouncedFetchAutocompleteSuggestions(value);
+
+        usedCallbackDebouncedFetchAutocompleteSuggestions(value);
 
         if (isRequiredError) {
           clearIsRequiredError();
